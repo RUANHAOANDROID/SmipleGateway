89fc2d3b7f3379ae319429c7e8bbe47b3d999194
阮皓 <52927295@qq.com>
2022-04-27 16:40:13 +0800

    配置信任的代理列表（默认信任所有应该避免）

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ func main() {
 	//配置GIN
 	r := StartRouter()
 	//启动UDP，注意先开线程避免无限阻塞
-	StartUDPServer("127.0.0.1:8088")
+	StartUDPServer("192.168.1.121:60000")
 	//启动GIN HTTP服务
-	r.Run("127.0.0.1:8088")
+	r.SetTrustedProxies([]string{"127.0.0.1", "localhost"})
+	r.Run(":8088")
 }
